7c358ee85aeabe034528ad211f0fa2c818f11763
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2022-05-25 21:53:15 -0300

    Criado banco H2, Entidade Client, Script de Inserção.

---
+++ backend/src/main/java/com/luisribeiro/crudcliente/entities/Client.java
@@ -0,0 +1,23 @@
+package com.luisribeiro.crudcliente.entities;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name ="Client")
+public class Client {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Instant birthDate;
+	private Integer children;
+
+}

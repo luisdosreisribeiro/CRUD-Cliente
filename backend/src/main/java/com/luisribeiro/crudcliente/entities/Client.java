c2090f0406a560e416c8abd99eb26245d229cf7b
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2022-05-26 23:39:42 -0300

    Revert "Criado endpoint para buscar todos os clientes"
    This reverts commit b16daf46bd3a99fc13dc94e60155e32296e7f4a7.

--- backend/src/main/java/com/luisribeiro/crudcliente/entities/Client.java
+++ backend/src/main/java/com/luisribeiro/crudcliente/entities/Client.java
@@ -1,6 +1,5 @@
 package com.luisribeiro.crudcliente.entities;
 
-import java.io.Serializable;
 import java.time.Instant;
 
 import javax.persistence.Entity;
@@ -10,8 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 @Entity
 @Table(name ="Client")
-public class Client implements Serializable{
-	private static final long serialVersionUID = 1L;
+public class Client {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,72 +20,4 @@ public class Client implements Serializable{
 	private Instant birthDate;
 	private Integer children;
 
-	public Client() {
-		
-	}
-	
-	
-
-	public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
-		this.id = id;
-		this.name = name;
-		this.cpf = cpf;
-		this.income = income;
-		this.birthDate = birthDate;
-		this.children = children;
-	}
-
-
-
-	public Long getId() {return id;}
-
-	public void setId(Long id) {this.id = id;}
-
-	public String getName() {return name;}
-
-	public void setName(String name) {this.name = name;}
-
-	public String getCpf() {return cpf;	}
-
-	public void setCpf(String cpf) {this.cpf = cpf;	}
-
-	public Double getIncome() {	return income;}
-
-	public void setIncome(Double income) {this.income = income;}
-
-	public Instant getBirthDate() {return birthDate;}
-
-	public void setBirthDate(Instant birthDate) {this.birthDate = birthDate;}
-
-	public Integer getChildren() {return children;}
-
-	public void setChildren(Integer children) {	this.children = children;}
-
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Client other = (Client) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	
 }
